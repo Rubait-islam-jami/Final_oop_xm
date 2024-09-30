04a55f14e781bc0f7fb1672c3b9fa89a504083ef
Rubait-islam-jami <146682245+Rubait-islam-jami@users.noreply.github.com>
2024-10-01 00:20:20 +0600

    Update oop_final.py

--- oop_final.py
+++ oop_final.py
@@ -1,15 +1,16 @@
 import uuid
 
 class Account:
-    def __init__(self,name,email,address,account_type):
+    def __init__(self,name,email,address,account_type,password,account_no):
         self.name=name
         self.email=email
         self.address=address
         self.account_type =account_type
+        self.password = password 
         self.balance=0
         self.loan_count=0
         self.transaction_history=[]
-        self.account_no = str(uuid.uuid4())
+        self.account_no = account_no
 
     def deposit(self,amount):
         self.balance +=amount
@@ -29,12 +30,17 @@ class Account:
         return self.transaction_history
     
     def take_loan(self,amount):
-        if self.loan_count <2:
-            self.balance +=amount
-            self.loan_count +=1
-            self.transaction_history.append(('Loan',amount))
+        if bank.loan_status:
+            if self.loan_count <2:
+              self.balance +=amount
+              self.loan_count +=1
+              bank.total_loan += amount
+              self.transaction_history.append(('Loan',amount))
+            else:
+               return"Loan limit exceeded"
         else:
-            return"Loan limit exceeded"
+           return "Loan disabled"
+    
         
     def transfer(self,amount,other_account):
         if amount >self.balance:
@@ -43,44 +49,6 @@ class Account:
             self.balance-=amount
             other_account.balance+=amount
             self.transaction_history.append(('Transfer',amount,other_account.account_no))
-
-    def  interact(self):
-        while True:
-            print("1.Deposit")
-            print("2. withdraw")
-            print("3. Check balance")
-            print("4. check transaction history")
-            print("5. take loan")
-            print("6. Transfer")
-            print("7.Exit")
-            choice= input("Enter your choice:")
-            if choice =='1':
-                amount= float(input("Enter amount:"))
-                self.deposit(amount)
-            elif choice=='2':
-                amount=float(input("Enter amount:"))
-                print(self.withdraw(amount))
-            elif choice=='3':
-                print("Balance:",self.check_balance())
-            elif choice=='4':
-                print("Transaction history:",self.check_transaction_history())
-            elif choice=='5':
-                amount=float(input("Enter amount :"))
-                print(self.take_loan(amount)) 
-            elif choice=='6':
-                amount= float(input("Enter amount :"))
-                other_account_no=input("Enter other account number:")
-                if other_account_no in bank.accounts:
-                     other_account= bank.accounts[other_account_no]
-                     print(self.transfer(amount,other_account))
-                else:
-                    print("Account does not exist")
-            elif choice=='7':
-                 break
-            else:
-                print("Invalid choice")
-
-
 class Bank:
     def __init__(self):
         self.accounts={}
@@ -88,13 +56,16 @@ class Bank:
         self.total_loan=0
         self.loan_status=True
 
-    def create_account(self,name,email,address,account_type):
-        new_account = Account(name,email,address,account_type)
+    def create_account(self,name,email,address,account_type,password,account_no):
+        new_account = Account(name,email,address,account_type,password,account_no)
         self.accounts[new_account.account_no] = new_account
-        
+        return account_no
+    
     def delete_account(self,account_no):
         if account_no in self.accounts:
             del self.accounts[account_no]
+        else:
+            print("Account does not exist")
         
     def show_users(self):
         for account_no,account in self.accounts.items():
@@ -107,27 +78,33 @@ class Bank:
         return total
     
     def total_loan(self):
-        total =0
-        for account in self.accounts.values():
-            total +=account.loan_count
-        return total
+        return self.total_loan
     
 
-    def interact(self): 
+    def admin_menu(self): 
         while True:
+            print("\nAdmin Menu:")
             print("1. Create account")
             print("2. Delete account")
             print("3. Show users")
             print("4. Total balance")
             print("5. Total loan")
-            print("6. Exit")
+            print("6. Enable/Disable loans")
+            print("7.logout")
             choice= input("Enter your choice:")
             if choice =='1':
                 name =input("Enter name:")
                 email= input("Enter email: ")
                 address =input("Enter address:")
-                account_type= input("Enter account type:")
-                self.create_account(name,email,address,account_type)
+                account_type= input("Enter account type (user/admin):")
+                password= input(" Enter password :")
+                account_no= input("enter new account number :")
+                if account_no in self.accounts:
+                    print("Already exists")
+
+                account_no = self.create_account(name,email,address,account_type,password)
+                print(f"Account created successfully.Account no:{account_no}")
+
             elif choice=='2':
                 account_no = input("Enter account number")
                 self.delete_account(account_no)
@@ -136,11 +113,103 @@ class Bank:
             elif choice =='4':
                 print("Total balance:",self.total_balance())
             elif choice=='5':
-                print("Total loan :",self.total_loan())
+                print("Total loan :",self.total_loan_amount())
             elif choice =='6':
+                self.loan_status = not self.loan_status
+                status ="enabled" if self.loan_status else "disabled"
+                print(f"Loan functionlity is now {status}")
+            elif choice == '7':
                 break
             else:
                 print("Invalid Choice")   
 
-bank=Bank()
+
+
+    def  user_menu(self,user_account):
+        while True:
+            print("\nUser Menu:")
+            print("1.Deposit")
+            print("2. withdraw")
+            print("3. Check balance")
+            print("4. check transaction history")
+            print("5. take loan")
+            print("6. Transfer")
+            print("7. Logout")
+            choice= input("Enter your choice:")
+            if choice =='1':
+                amount= float(input("Enter amount:"))
+                user_account.deposit(amount)
+            elif choice=='2':
+                amount=float(input("Enter amount:"))
+                print(user_account.withdraw(amount))
+            elif choice=='3':
+                print("Balance:",user_account.check_balance())
+            elif choice=='4':
+                print("Transaction history:",user_account.check_transaction_history())
+            elif choice=='5':
+                amount=float(input("Enter amount :"))
+                print(user_account.take_loan(amount)) 
+            elif choice=='6':
+                amount= float(input("Enter amount :"))
+                other_account_no=input("Enter other account number:")
+                if other_account_no in self.accounts:
+                     other_account= self.accounts[other_account_no]
+                     print(user_account.transfer(amount,other_account))
+                else:
+                    print("Account does not exist")
+            elif choice=='7':
+                 break
+            else:
+                print("Invalid choice")
+
+    def login(self):
+        account_no = input("Enter account number :")
+        password = input("Enter password: ")
+
+
+        if account_no in self.accounts:
+           account=self.accounts[account_no]
+           if account.password==password:
+                if account.account_type == 'admin':
+                   print("Admin logged IN")
+                   self.admin_menu()
+                else:
+                   print(f"User {account.name} logged in successfully")
+                   self.user_menu(account)
+           else:
+                print("Incorrect password")
+        else:
+           print("Account not found")
+
+    def register(self):
+        name =input("Enter name:")
+        email= input("Enter email: ")
+        address =input("Enter address:")
+        account_type= input("Enter account type (user/admin):")
+        password= input(" Enter password :")
+        account_no= input("Enter A New Account No :")
+
+        if account_no in self.accounts:
+            print("Already exists") 
+
+        account_no = self.create_account(name,email,address,account_type,password,account_no)
+        print(f"Account created successfully.Account no:{account_no}")
+
+    def interact(self):
+        while True:
+           print("\nWelcome to the Bank System")
+           print("1. Login")
+           print("2. Register")
+           print("3. Exit")
+           choice = input("Enter your choice: ")
+           if choice == '1':
+              self.login()
+           elif choice =='2':
+                self.register()
+           elif choice == '3':
+              break
+           else:
+             print("Invalid choice")
+
+bank = Bank()
 bank.interact()
